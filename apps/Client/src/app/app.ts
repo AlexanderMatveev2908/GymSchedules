c8e319c8991ef4d3ed96ae9762d92d1ff0b5066b
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2026-06-06 21:23:24 +0300

    sidebar => _

--- apps/Client/src/app/app.ts
+++ apps/Client/src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Toast } from '@/layout/toast/toast';
 import { WakeUp } from '@/layout/wake_up/wake-up';
 import { Navbar } from '@/layout/navbar/navbar';
+import { Sidebar } from '@/layout/sidebar/sidebar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Toast, WakeUp, Navbar],
+  imports: [RouterOutlet, Toast, WakeUp, Navbar, Sidebar],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
---
+++ apps/Client/src/layout/sidebar/sidebar.ts
@@ -0,0 +1,18 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { BgBlack } from '../bg_black/bg-black';
+import { SidebarSlice } from '@/features/sidebar/slice';
+import { LinksNonLoggedUiFct } from './etc/ui_fct';
+import { RouterLink } from '@angular/router';
+import { NgClass } from '@angular/common';
+
+@Component({
+  selector: 'app-sidebar',
+  imports: [BgBlack, RouterLink, NgClass],
+  templateUrl: './sidebar.html',
+  styleUrl: './sidebar.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Sidebar {
+  public readonly sidebarSlice: SidebarSlice = inject(SidebarSlice);
+  public readonly linksNonLoggedUiFct = LinksNonLoggedUiFct;
+}
