b0087156052474ace8a9d1b3858ec64fc9712cc3
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2026-06-06 21:37:43 +0300

    auth_layout => _

--- apps/Client/src/app/app.routes.ts
+++ apps/Client/src/app/app.routes.ts
@@ -1,3 +1,5 @@
+import { AuthLayout } from '@/pages/auth_layout/auth-layout';
+import { RegisterPage } from '@/pages/auth_layout/register_page/register-page';
 import { HomePage } from '@/pages/home_page/home-page';
 import { NotFoundPage } from '@/pages/not_found_page/not-found-page';
 import { NoticePage } from '@/pages/notice_page/notice-page';
@@ -11,6 +13,16 @@ export const routes: Routes = [
   {
     path: 'notice',
     component: NoticePage,
+  },
+  {
+    path: 'auth',
+    component: AuthLayout,
+    children: [
+      {
+        path: 'register',
+        component: RegisterPage,
+      },
+    ],
   },
   {
     path: '**',
