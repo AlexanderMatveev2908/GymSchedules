3ad527ae839b237fcb4045319259399cde2fe564
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2026-06-06 13:32:58 +0300

    main => __

--- apps/Client/src/environments/environment.ts
+++ apps/Client/src/environments/environment.ts
@@ -5,7 +5,7 @@ export class EnvVars {
   public static readonly backURLDev: string = 'https://localhost/api/v1';
   public static readonly backURL: string = 'https://gym-schedules-server.fly.dev/api/v1';
   public static readonly frontURLDev: string = 'https://localhost';
-  public static readonly frontURL: string = 'https://invoices-app-client.fly.dev';
+  public static readonly frontURL: string = 'https://gym-schedules-client.fly.dev';
 
   public static isDev(): boolean {
     return this.mode === 'development';
