0a050aa78772c6d1a2bffb5efd3607f71a51d3a1
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2026-06-07 13:44:26 +0300

    server_hashing => _

--- apps/Server/Features/Auth/Controllers.cs
+++ apps/Server/Features/Auth/Controllers.cs
@@ -4,6 +4,7 @@ using InvoicesApp.ModelsNS.UsersNS;
 using InvoicesApp.TypesNS.UsersNS;
 using Microsoft.EntityFrameworkCore;
 using Server.ConfigNS.SqlNS;
+using Server.LibNS.JwtNS;
 
 namespace Server.FeaturesNS.AuthNS;
 
@@ -21,13 +22,19 @@ public static class AuthCtrl
       return Res.Json(404, "User already exists");
 
     Users newUser = new(dto);
+    newUser.Password = BCrypt.Net.BCrypt.HashPassword(
+    dto.Password
+);
 
     db.Users.Add(newUser);
     await db.SaveChangesAsync();
 
+    string token = JwtLib.Create(newUser);
+
     return Res.Json(201, "User registered",
   new
   {
+    accessToken = token,
     newUser = LibShape.Merge(newUser, new
     {
       id = newUser.Id
