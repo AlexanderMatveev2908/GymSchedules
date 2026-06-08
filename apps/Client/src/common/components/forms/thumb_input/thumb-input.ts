da9c6b73519a804351313f4b466598497ae5e729
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2026-06-08 14:37:27 +0300

    profile_page => _

--- apps/Client/src/common/components/forms/thumb_input/thumb-input.ts
+++ apps/Client/src/common/components/forms/thumb_input/thumb-input.ts
@@ -1,16 +1,5 @@
-import { FormFieldT } from '@/common/types/dom';
-import { Nullable, OrNone } from '@/common/types/general';
-import { UseFormFieldDir } from '@/core/directives/use_form_field_dir';
-import { LibLog } from '@/core/lib/log';
-import {
-  ChangeDetectionStrategy,
-  Component,
-  computed,
-  input,
-  InputSignal,
-  OnInit,
-  Signal,
-} from '@angular/core';
+import { UseFormFieldImg } from '@/core/directives/use_form_field_img';
+import { ChangeDetectionStrategy, Component, OnInit, Signal } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
@@ -20,42 +9,7 @@ import { ReactiveFormsModule } from '@angular/forms';
   styleUrl: './thumb-input.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ThumbInput extends UseFormFieldDir implements OnInit {
-  public readonly field: InputSignal<FormFieldT> = input.required();
-
-  private isFile(val: unknown): val is File {
-    return val instanceof File;
-  }
-
-  public readonly srcImg: Signal<Nullable<string>> = computed(() => {
-    const val = this.val?.();
-
-    if (typeof val === 'string') {
-      return val;
-    }
-
-    if (this.isFile(val)) {
-      return URL.createObjectURL(val);
-    }
-
-    return null;
-  });
-
-  public handleFileChange(e: Event): void {
-    const input = e.target as HTMLInputElement;
-    const file = input.files?.[0] ?? null;
-
-    if (!file) return;
-
-    const c = this.ctrl();
-    c.markAsDirty();
-    c.markAsTouched();
-    c.setValue(file);
-
-    console.log(file);
-    LibLog.main('banana', this.val?.());
-  }
-
+export class ThumbInput extends UseFormFieldImg implements OnInit {
   ngOnInit(): void {
     this.setupField();
   }
